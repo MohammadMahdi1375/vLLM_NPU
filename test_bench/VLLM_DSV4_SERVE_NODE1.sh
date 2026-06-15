ff71068db1002dedd8d50290a81409150030aea8
m84379596 <mohammad.mahdi.moradi@h-partners.com>
2026-06-16 00:03:45 +0800

    modify inference .sh

--- test_bench/VLLM_DSV4_SERVE_NODE1.sh
+++ test_bench/VLLM_DSV4_SERVE_NODE1.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-set -eo pipefail
-
 cd /home/n84449292/m84379596/DFlash/vLLM_NPU/vllm
 
 source /home/n84449292/m84379596/CANN/CANN9.0.0/ascend-toolkit/set_env.sh
@@ -13,41 +10,23 @@ sleep 3
 
 export PYTHONPATH=/home/n84449292/m84379596/DFlash/vLLM_NPU/vllm:/home/n84449292/m84379596/DFlash/vLLM_NPU/vllm-ascend:$PYTHONPATH
 export VLLM_USE_V1=1
-export VLLM_LOGGING_LEVEL=INFO
-
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export HCCL_CONNECT_TIMEOUT=1800
 
 export MASTER_ADDR=80.5.5.108
 export MASTER_PORT=29501
+export HCCL_CONNECT_TIMEOUT=1800
 
-export GLOO_SOCKET_IFNAME="$(ip -o -4 addr show | awk '/80\.5\.5\./{print $2; exit}')"
-export HCCL_SOCKET_IFNAME="$GLOO_SOCKET_IFNAME"
-export TP_SOCKET_IFNAME="$GLOO_SOCKET_IFNAME"
-export NCCL_SOCKET_IFNAME="$GLOO_SOCKET_IFNAME"
+export GLOO_SOCKET_IFNAME=$(ip -o -4 addr show | awk '/80\.5\.5\./{print $2; exit}')
+export HCCL_SOCKET_IFNAME=$GLOO_SOCKET_IFNAME
+export TP_SOCKET_IFNAME=$GLOO_SOCKET_IFNAME
 export GLOO_USE_IPV6=0
 
-export no_proxy="localhost,127.0.0.1,80.5.5.108,80.5.5.109"
-export NO_PROXY="$no_proxy"
-
-echo "===== NODE 1 ENV ====="
-echo "PWD=$(pwd)"
-echo "IFACE=$GLOO_SOCKET_IFNAME"
-ip -o -4 addr show dev "$GLOO_SOCKET_IFNAME"
-python - <<'PY'
-import vllm
-print("vllm file:", getattr(vllm, "__file__", None))
-from vllm import SamplingParams
-print("SamplingParams OK:", SamplingParams)
-PY
-
 MODEL=/home/n84449292/m84379596/Huggingface/DeepSeek-V4-Flash-bf16
-LOG=dsv4_vllm_node109_raw.log
 
 vllm serve "$MODEL" \
   --trust-remote-code \
-  --tensor-parallel-size 8 \
-  --pipeline-parallel-size 2 \
+  --tensor-parallel-size 16 \
+  --pipeline-parallel-size 1 \
   --nnodes 2 \
   --node-rank 1 \
   --master-addr 80.5.5.108 \
@@ -55,5 +34,4 @@ vllm serve "$MODEL" \
   --headless \
   --enable-expert-parallel \
   --max-num-seqs 1 \
-  --max-model-len 2048 \
-  2>&1 | tee "$LOG"
+  --max-model-len 2048
