fa64308c626dedc02724159ad36d687f0aa3e4f7
m84379596 <mohammad.mahdi.moradi@h-partners.com>
2026-06-16 03:15:58 +0800

    adding mem-fract

--- test_bench/VLLM_DSV4_SERVE_NODE0.sh
+++ test_bench/VLLM_DSV4_SERVE_NODE0.sh
@@ -38,7 +38,7 @@ vllm serve "$MODEL" \
   --enable-expert-parallel \
   --max-num-seqs 1 \
   --max-model-len 2048 \
-  --gpu-memory-utilization 0.75 \
+  --gpu-memory-utilization 0.8 \
   --block-size 128 \
   --max-num-batched-tokens 2048 \
   --no-enable-prefix-caching \
